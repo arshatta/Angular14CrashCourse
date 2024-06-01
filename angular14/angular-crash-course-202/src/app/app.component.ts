5bc860d52e9feae8668c82e4106e624e41ef789d
Talap Arshat <103632834+arshatta@users.noreply.github.com>
2024-06-01 16:21:30 +0500

    Add files via upload

---
+++ angular14/angular-crash-course-202/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./models/product";
+import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
+import {ModalService} from "./services/modal.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent{
+
+}
